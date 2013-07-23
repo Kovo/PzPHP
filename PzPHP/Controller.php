ae15a2d5da6e7e2412bdd709a9235595633b429d
Kevork <kovo88@gmail.com>
2013-07-22 23:33:12 -0400

    added controller support

--- PzPHP/Controller.php
+++ PzPHP/Controller.php
@@ -1,7 +1,20 @@
 <?php
-	class PzPHP_Controller extends PzPHP_Wrapper
+	class PzPHP_Controller
 	{
+		/**
+		 * @var null|PzPHP_Core
+		 */
+		protected $_PZPHP = NULL;
+
 		public function before(){}
+
+		/**
+		 * @param PzPHP_Core $PzPHPCore
+		 */
+		function __construct(PzPHP_Core $PzPHPCore)
+		{
+			$this->_PZPHP = $PzPHPCore;
+		}
 
 		public function after(){}
 	}
